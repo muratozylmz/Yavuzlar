a1f431917a787b123e8eb0412d4613ed6e192195
Murat Özyılmaz <eparis@atlassian.com>
2024-12-17 14:46:28 +0300

    1. Site Eklendi

---
+++ TASK_7_WEB_SCRAPER/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/gocolly/colly"
+)
+
+func main() {
+	siteSelection := flag.String("site", "", "Haber sitesi seçmenizi sağlar. \n Örnek Kullanım: -site '1 veya 2' \n\n 1 - The Hacker News \n 2 - Ajansspor")
+	dateFilter := flag.Bool("date", false, "Tarih bilgilerini göster.\n Örnek Kullanım: -site '1 veya 2' -date\n")
+	descriptionFilter := flag.Bool("desc", false, "Acıklama bilgilerini göster.\n Örnek Kullanım: -site '1 veya 2' -desc\n")
+	flag.Parse()
+
+	if *siteSelection == "" {
+		log.Fatal("Eksik parametre gönderildi.")
+	}
+	sites := strings.Split(*siteSelection, ",")
+
+	for _, site := range sites {
+		switch site {
+		case "1":
+			getNews1("https://thehackernews.com/", *dateFilter, *descriptionFilter)
+		case "2":
+			getNews2("https://ajansspor.com/", *dateFilter, *descriptionFilter)
+		default:
+			log.Fatal("Gecersiz site seçildi.")
+		}
+
+	}
+
+}
+
+func getNews1(url string, dateFilter bool, descriptionFilter bool) {
+	c := colly.NewCollector(colly.AllowedDomains("thehackernews.com"))
+
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Printf("Error: %s\n", err.Error))
+	})
+
+	c.OnHTML("div.body-post", func(e *colly.HTMLElement) {
+		selection := h.DOMerr
+
+		title := selection.Find("h2.home-title").Text()
+		date := selection.Find("span.h-datetime").Text()
+		trimmedDate := date[3:]
+		description := selection.Find("div.home-desc").Text()
+		newsLink := selection.Find("a.story-link").AttrOr("href", "")
+		if !dateFilter && !descriptionFilter {
+			fmt.Printf("Haber Başlığı: %s\n\n Tarih: %s\n\n Açıklama: %s...\n\n Haber Linki: %s\n\n, title, trimmedDate, description, newsLink)
+		}
+		else if dateFilter && !descriptionFilter {
+			fmt.Printf("Haber Başlığı: %s\n\n Tarih: %s\n\n Haber Linki: %s\n\n, title, trimmedDate, newsLink)
+		}
+		else if !dateFilter && descriptionFilter {
+			fmt.Printf("Haber Başlığı: %s\n\n Açıklama: %s...\n\n Haber Linki: %s\n\n, title, description, newsLink)
+		}
+		else {
+			fmt.Printf("Haber Başlığı: %s\n\n Tarih: %s\n\n Açıklama: %s...\n\n Haber Linki: %s\n\n, title, trimmedDate, description, newsLink)
+		}
+	})
+
+	c.Visit(url)
+}
+
