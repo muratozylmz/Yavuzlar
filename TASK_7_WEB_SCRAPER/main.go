f1ff5521606b5c575fead1ce6740dedcd141ba83
Murat Özyılmaz <muratozylmz27@gmail.com>
2024-12-17 16:33:07 +0300

    2. Site Eklendi

--- TASK_7_WEB_SCRAPER/main.go
+++ TASK_7_WEB_SCRAPER/main.go
@@ -16,7 +16,7 @@ func main() {
 	flag.Parse()
 
 	if *siteSelection == "" {
-		log.Fatal("Eksik parametre gönderildi.")
+		log.Fatal("Eksik parametre gönderildi. Açıklamalar ve örnek kullanım için lütfen '-h' parametresini kullanın.")
 	}
 	sites := strings.Split(*siteSelection, ",")
 
@@ -38,11 +38,11 @@ func getNews1(url string, dateFilter bool, descriptionFilter bool) {
 	c := colly.NewCollector(colly.AllowedDomains("thehackernews.com"))
 
 	c.OnError(func(r *colly.Response, err error) {
-		fmt.Printf("Error: %s\n", err.Error))
+		fmt.Printf("Error: %s\n", err.Error())
 	})
 
-	c.OnHTML("div.body-post", func(e *colly.HTMLElement) {
-		selection := h.DOMerr
+	c.OnHTML("div.body-post", func(h *colly.HTMLElement) {
+		selection := h.DOM
 
 		title := selection.Find("h2.home-title").Text()
 		date := selection.Find("span.h-datetime").Text()
@@ -50,19 +50,48 @@ func getNews1(url string, dateFilter bool, descriptionFilter bool) {
 		description := selection.Find("div.home-desc").Text()
 		newsLink := selection.Find("a.story-link").AttrOr("href", "")
 		if !dateFilter && !descriptionFilter {
-			fmt.Printf("Haber Başlığı: %s\n\n Tarih: %s\n\n Açıklama: %s...\n\n Haber Linki: %s\n\n, title, trimmedDate, description, newsLink)
-		}
-		else if dateFilter && !descriptionFilter {
-			fmt.Printf("Haber Başlığı: %s\n\n Tarih: %s\n\n Haber Linki: %s\n\n, title, trimmedDate, newsLink)
-		}
-		else if !dateFilter && descriptionFilter {
-			fmt.Printf("Haber Başlığı: %s\n\n Açıklama: %s...\n\n Haber Linki: %s\n\n, title, description, newsLink)
-		}
-		else {
-			fmt.Printf("Haber Başlığı: %s\n\n Tarih: %s\n\n Açıklama: %s...\n\n Haber Linki: %s\n\n, title, trimmedDate, description, newsLink)
+			fmt.Printf("Haber Başlığı: %s\n\n Tarih: %s\n\n Açıklama: %s...\n\n Haber Linki: %s\n\n", title, trimmedDate, description, newsLink)
+		} else if dateFilter && !descriptionFilter {
+			fmt.Printf("Haber Başlığı: %s\n\n Tarih: %s\n\n Haber Linki: %s\n\n", title, trimmedDate, newsLink)
+		} else if !dateFilter && descriptionFilter {
+			fmt.Printf("Haber Başlığı: %s\n\n Açıklama: %s...\n\n Haber Linki: %s\n\n", title, description, newsLink)
+		} else {
+			fmt.Printf("Haber Başlığı: %s\n\n Tarih: %s\n\n Açıklama: %s...\n\n Haber Linki: %s\n\n", title, trimmedDate, description, newsLink)
 		}
 	})
 
 	c.Visit(url)
 }
 
+func getNews2(scrapeURL string, dateFilter bool, descriptionFilter bool) {
+	c := colly.NewCollector(colly.AllowedDomains("ajansspor.com"))
+
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Printf("Error: %s\n", err.Error())
+	})
+
+	c.OnHTML("div.card", func(h *colly.HTMLElement) {
+		selection := h.DOM
+
+		title := selection.Find("div.news-title").Text()
+
+		date := selection.Find("div.news-date-bottom").Text()
+		trimmedDate := date
+
+		description := selection.Find("div.post-content").Text()
+
+		newsLink := selection.Find("a").AttrOr("href", "")
+
+		if !dateFilter && !descriptionFilter {
+			fmt.Printf("Haber Başlığı: %s\n\n Tarih: %s\n\n  Açıklama: %s...\n\n Haber Linki: %s\n\n", title, trimmedDate, description, newsLink)
+		} else if dateFilter && !descriptionFilter {
+			fmt.Printf("Haber Başlığı: %s\n\n Haber Linki: %s\n\n", title, newsLink)
+		} else if !dateFilter && descriptionFilter {
+			fmt.Printf("Haber Başlığı: %s\n\n Tarih: %s\n\n ", title, trimmedDate)
+		} else {
+			fmt.Printf("Haber Başlığı: %s\n\n", title)
+		}
+	})
+
+	c.Visit(scrapeURL)
+}
